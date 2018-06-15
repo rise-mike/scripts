107713632bff9a4adeeafb02fb496599aaaa3b38
rise-mike <mike.morganti@risepeople.com>
2018-06-15 14:26:57 -0700

    Adding JSON > csv conversion script

---
+++ json_csv.py
@@ -0,0 +1,24 @@
+# Every letter is in it's own cell...but the rows work?
+
+from sys import argv
+import json
+import csv
+
+infile = input("In this directory, what's your JSON file called? > ")
+outfile = input("What do you want the destination (with extension) to be? > ")
+print()
+input(f'So you want {infile} to wind up in {outfile}?')
+
+with open(infile) as source:
+  data = json.load(source)
+
+destination = open(outfile, 'w')
+
+writer = csv.writer(destination)
+
+writer.writerow(["Name"] + ["Text"])
+
+for k, v in data['TEXT'].items():
+  this_row = k + ":" + v
+  writer.writerow([k] + [v])
+
